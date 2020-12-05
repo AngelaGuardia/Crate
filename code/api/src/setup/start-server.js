e84b16dc432a9013cec766ac44a08c12357afa62
Angela Guardia <angelaguardia20@gmail.com>
2020-12-04 17:50:41 -0800

    2. Server start-up

--- code/api/src/setup/start-server.js
+++ code/api/src/setup/start-server.js
@@ -1,7 +1,9 @@
+// NOTE: 2. This is where the server is started
 // App Imports
 import { PORT, NODE_ENV } from '../config/env'
 
 // Start server
+// QUESTION: Does this 'default fuction' mean it gets the name of the directory and the file in cammel case? That's how it is being called in the index.js file
 export default function (server) {
   console.info('SETUP - Starting server..')
 
@@ -13,3 +15,5 @@ export default function (server) {
     }
   })
 }
+
+// QUESTION: Why do we need an almost identical file in the web directory?
