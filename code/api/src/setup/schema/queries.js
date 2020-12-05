3b5ba605d5df5c02f9b4fe0676c5337589fb3c68
Angela Guardia <angelaguardia20@gmail.com>
2020-12-04 17:52:06 -0800

    5. Queries Setup

--- code/api/src/setup/schema/queries.js
+++ code/api/src/setup/schema/queries.js
@@ -1,13 +1,16 @@
+// NOTE: 5. This is where we start to define queries
 // Imports
 import { GraphQLObjectType } from 'graphql'
 
 // App Imports
+// NOTE: queries are imported for each type of resource
 import * as user from '../../modules/user/query'
 import * as product from '../../modules/product/query'
 import * as crate from '../../modules/crate/query'
 import * as subscription from '../../modules/subscription/query'
 
 // Query
+// NOTE: The main query object is defined here. It includes queries from all resources.
 const query = new GraphQLObjectType({
   name: 'query',
   description: 'API Queries [Read]',
