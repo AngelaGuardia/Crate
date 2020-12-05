aac34d3f3c7032684be143ad186509b53d5c43b6
Angela Guardia <angelaguardia20@gmail.com>
2020-12-04 17:51:13 -0800

    3. GraphQL setup

--- code/api/src/setup/graphql.js
+++ code/api/src/setup/graphql.js
@@ -1,10 +1,15 @@
+// NOTE: 3. This is where graphql is being set up
 // Imports
 import graphqlHTTP from 'express-graphql'
 
 // App Imports
 import serverConfig from '../config/server.json'
+
 import authentication from './authentication'
+// NOTE: This is the first place where we use authentication. We will need access to this endpoint without authentication in some cases or we may need to create a new endpoint.
+
 import schema from './schema'
+// QUESTION: How is it importing a variable from an entire folder like this?
 
 // Setup GraphQL
 export default function (server) {
