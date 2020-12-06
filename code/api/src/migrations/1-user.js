d0981b309d261ee1bad24a8c34c75b24a4ea941f
Cam C <ckc.cameron@gmail.com>
2020-12-05 20:36:15 -0800

    Annotate migration for creating Users table
    * notes about the specific User migration as well as ideas on how 
    migration files will look to add the data columns that we'll need

--- code/api/src/migrations/1-user.js
+++ code/api/src/migrations/1-user.js
@@ -1,3 +1,14 @@
+// Migrations have to have an up and a down. This one specifically creates the
+// Users table with Sequelize as the ORM.
+// It seems like we'll have to create new migration to add image, description
+// and address columns to the Users table, perhaps using
+// addColumn('users'...etc...). Also add nextDeliveryDate to Subscriptions for
+// the adjusting availability piece of this project track.
+// We will generate new migrations to create tables for productHistory and
+// crateProducts, which will act as joins tables for us. The productHistory can
+// associate users with their products and have a column like kept? to set a
+// boolean on whether or not the user kept the item from the crate.
+// The crateProducts can associate crates with the products they contain.
 module.exports = {
   up: (queryInterface, Sequelize) => {
     return queryInterface.createTable('users', {
