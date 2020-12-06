0a37bfd56ac60d9abf9bebf8efe1a60c6dcad367
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-12-06 09:24:49 -0700

    Add annotations for SET_USER and LOGOUT in state.js

--- code/web/src/modules/user/api/state.js
+++ code/web/src/modules/user/api/state.js
@@ -13,10 +13,24 @@ export const userInitialState = {
 // State
 export default (state = userInitialState, action) => {
   switch (action.type) {
+    /*for case SET_USER, must add new properties that feature depends on.
+      for instance, after details: actions.user, the next line should have
+      history with the product history retrieved from the API
+      perhaps we should add history, description, shippingAddress, etc
+      to details key so that these details exist where email already does 
+    */  
     case SET_USER:
       return {
+        //spread operator to return whatever currently exists in state
         ...state,
+        /*line  below will set the value for isAuthenticated
+          - to true if isEmpty() from helpers.js returns false, 
+          given the user object from action creater
+          - to false if isEmpty() returns true
+        */
         isAuthenticated: !isEmpty(action.user),
+        //details key is assigned a value of the user property in the action object
+        //comes from the action creator of setUser()
         details: action.user,
       }
 
@@ -34,7 +48,12 @@ export default (state = userInitialState, action) => {
         isLoading: false
       }
 
+    /*For case LOGOUT, new properties such as 
+      availability, history, description, shippingAddress, and image
+      will need to be set to null or empty arrays depending on the data type
+    */
     case LOGOUT:
+      //this case will reset the user state with this updated properties
       return {
         ...state,
         error: null,
@@ -42,7 +61,9 @@ export default (state = userInitialState, action) => {
         isAuthenticated: false,
         details: null
       }
-
+    //default case will return state as it exists without any modifications
+    //this is set up to avoid returning undefined upon initial load and
+    //to return state when action.type is not specified
     default:
       return state
   }
