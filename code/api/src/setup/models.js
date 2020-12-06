1b0b8cb2c3bfd1a63ba2eb868cca3ab23407cd34
Cam C <ckc.cameron@gmail.com>
2020-12-06 14:07:40 -0800

    Annotate setup file for models

--- code/api/src/setup/models.js
+++ code/api/src/setup/models.js
@@ -4,6 +4,7 @@ import Sequelize from 'sequelize'
 // App Imports
 import databaseConnection from './database'
 
+// Lays out each model and completes connection to database
 const models = {
   User: databaseConnection.import('../modules/user/model'),
   Product: databaseConnection.import('../modules/product/model'),
@@ -11,6 +12,9 @@ const models = {
   Subscription: databaseConnection.import('../modules/subscription/model')
 }
 
+// This creates model relationships and associations, essentially assigning
+// indexes/keys to models based on how relationships are defined within
+// individual models.
 Object.keys(models).forEach(modelName => {
   if (models[modelName].associate) {
     models[modelName].associate(models)
