c982b04757ba48833a5824bc8fba760deae1b8f9
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-12-07 02:41:55 -0500

    Add routing annotation

--- code/web/src/setup/client/App.js
+++ code/web/src/setup/client/App.js
@@ -8,11 +8,30 @@ import Layout from '../../modules/common/Layout'
 import NotFound from '../../modules/common/NotFound'
 import RoutePrivate from '../../modules/auth/RoutePrivate'
 
+
+/* 
+ Imports all routes from the routes folder
+ each route is an object containing
+ {
+ auth: boolean to determine if needs to be logged in to access,
+ path: utilized by the switch to determine if that is the correct path (is either function or string),
+ role: this may or may not exist, is either ADMIN or non-existant from what I have seen
+ }
+  */
+
+/*
+ routes === {
+ dashboard: {auth:true, path:/admin/dashboard, component:Dashboard, role:ADMIN},
+ profile: {auth: true, path: /user/profile component:Profile}
+ }
+*/
+
+console.log(routes)
 const App = () => (
   <Layout>
     <Switch>
       {Object.values(routes).map((route, index) => (
-        route.auth
+        route.auth // If route.auth is true, render the private component, else render the regular route component   
           ? <RoutePrivate {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
           : <Route {...route} key={index} path={typeof route.path === 'function' ? route.path() : route.path}/>
       ))}
