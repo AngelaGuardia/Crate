4fda6bdd37a304c11ac31c6fc90a24b847fd1052
Nicole Gooden <nicolemgooden.dev@gmail.com>
2020-12-07 11:40:43 -0700

    Add styling for availability input and associated text

--- code/web/src/modules/availability/Availability.js
+++ code/web/src/modules/availability/Availability.js
@@ -5,11 +5,17 @@ export const Availability = () => {
   let minDate = moment().format('YYYY-MM-DD');
   let maxDate = moment().add(7, 'days').format('YYYY-MM-DD');
   return (
-    <article>
-      <label htmlFor='select-availability'>
-        Select Availability:
-        <input type='date' min={minDate} max={maxDate}/>
-      </label>
-    </article>
+    <section style={{ display: 'flex', justifyContent: 'center' }}>
+      <article style={{ paddingRight: '2em' }}>
+        <h3>Next Delivery: </h3>
+        <h4>Available?</h4>
+      </article>
+      <article>
+        <label htmlFor='select-availability'>
+          Select Availability:
+          <input type='date' min={minDate} max={maxDate}/>
+        </label>
+      </article>
+    </section>
   )   
 }
