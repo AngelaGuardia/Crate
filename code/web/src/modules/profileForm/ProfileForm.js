a3c4ea8f308d5ab2641a4636a521002f8bedd52d
Ian Holladay <holladayian@gmail.com>
2020-12-08 14:33:50 -0900

    create a class that conditionally renders a form

--- code/web/src/modules/profileForm/ProfileForm.js
+++ code/web/src/modules/profileForm/ProfileForm.js
@@ -1,38 +1,70 @@
 import React from 'react';
+import { Component } from 'react';
 import { connect } from 'react-redux';
-import { updateAvailability } from '../subscription/api/actions';
+import Button from '../../ui/button';
+import { saveProfile } from '../user/api/actions';
+import { changeEditMode } from '../user/api/actions';
 
-const ProfileForm = (props) => {
+// import { updateAvailability } from '../subscription/api/actions';
 
+class ProfileForm extends Component{
+  constructor(props) {
+    super(props)
 
-  return (
-    <section style={{ display: 'flex', justifyContent: 'center' }}>
-      {props.user.isEditMode === true ?
-        (
-          <article>  
-            <img src={props.user.img}></img>
-            <input onChange={event => props.updateAvailability(event.target.value)} type='address'>{props.user.address}</input>
-            <input onChange={event => props.updateAvailability(event.target.value)} type='description'>{props.user.description}</input>
-          </article>
-        ) :
-        (
-          <article>  
-            <img src={props.user.img}></img>
-            <p>{props.user.address}</p>
-            <p>{props.user.description}</p>
-          </article>
-        )}
-    </section>
-  )   
-}
+      this.state = {
+        img: '',
+        address: '',
+        email: '',
+        description: ''
+      }
+  }
 
-function availabilityState(state) {
+  updateProfileState(e) {
+    console.log('this, updateProfileState', this)
+    this.setState({ [e.target.name]: e.target.value })
+  }
+
+  updateProfile() {
+    saveProfile(this.state)
+    changeEditMode(this.props.user)
+  }
+
+  render() {
+      return (
+        <section style={{ display: 'flex', justifyContent: 'center' }}>
+        {this.props.user.isEditMode ?
+          (
+            <article>  
+              {/* look into image file/upload */}
+              <input onChange={e => this.updateProfileState(e)} name='img' value={this.props.user.details.img} />
+              <input onChange={e => this.updateProfileState(e)} name='address' value={this.props.user.details.address} />
+              <input onChange={e => this.updateProfileState(e)} name='email' value={this.props.user.details.email} />
+              <input onChange={e => this.updateProfileState(e)} name='description' value={this.props.user.details.description} />
+              {/* change the name of changeEditMode or use conditionals */}
+              <Button onClick={this.updateProfile} theme="secondary">Save Profile</Button>
+            </article>
+          ) :
+          (
+            <article>  
+              <img src={this.props.user.details.img}></img>
+              <p>{this.props.user.details.address}</p>
+              <p>{this.props.user.details.email}</p>
+              <p>{this.props.user.details.description}</p>
+            </article>
+          )}
+        </section>
+      )   
+    }
+  }
+
+function profileFormState(state) {
     return {
       user: state.user
     }
   }
 
-export default connect(availabilityState, { updateAvailability })(Availability)
+
+export default connect(profileFormState, { saveProfile, changeEditMode })(ProfileForm)
 
 /*
 this.state = {
