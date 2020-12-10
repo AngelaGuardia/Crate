a2dbf45ed6cd67d5fc671484c305e53c2127b404
Angela Guardia <angelaguardia20@gmail.com>
2020-12-10 14:53:44 -0800

    User Query Tests

--- code/api/src/modules/user/tests/userQuery.test.js
+++ code/api/src/modules/user/tests/userQuery.test.js
@@ -21,27 +21,56 @@ describe('User Queries', () => {
       .post('/graphql')
       .send({query: `{ users { email } }`})
       .expect(200)
-    console.log(response.body)
-    expect(response.body.data.users.length).toBe(2);
+    expect(response.body.data.users.length).toBe(5);
     done();
   })
 
   it('returns a user based on id', async(done) => {
     const response = await request(server)
       .post('/graphql')
-      .send({query: `{user(id:1) { email name role createdAt } }`})
+      .send({query: `{user(id:1) { email name role } }`})
       .expect(200)
     expect(response.body).toMatchObject({
       data:{
         user: {
           email: 'admin@crate.com',
           name: 'The Admin',
-          role: 'ADMIN',
-          createdAt: '1607446274409'
+          role: 'ADMIN'
         }
       }
     });
     expect(response.body.data.user.email).toBe('admin@crate.com');
+    done();
+  })
+
+  it('returns user genders', async(done) => {
+    const response = await request(server)
+      .post('/graphql')
+      .send({query: `{ userGenders { id name } }`})
+      .expect(200)
+    expect(response.body).toMatchObject({
+      data: {
+        userGenders: [
+          {
+            id: 1,
+            name: "Male"
+          },
+          {
+            id: 2,
+            name: "Female"
+          }
+        ]
+      }
+    });
+    done();
+  })
+
+  it('returns userLogin token', async(done) => {
+    const response = await request(server)
+      .post('/graphql')
+      .send({query: `{userLogin(email: "admin@crate.com", password: "123456") {token}}`})
+      .expect(200)
+    expect(typeof response.body.data.userLogin.token).toBe('string');
     done();
   })
 })
