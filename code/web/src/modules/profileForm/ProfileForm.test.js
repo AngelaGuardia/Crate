a1a1d0cb40ca94585d956289a01d51a8ed55debc
Ian Holladay <holladayian@gmail.com>
2020-12-10 20:25:52 -0900

    test that ProfileForm renders

--- code/web/src/modules/profileForm/ProfileForm.test.js
+++ code/web/src/modules/profileForm/ProfileForm.test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { getAllByRole, render, screen, waitFor } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import {memoryRouter, Router } from 'react-router-dom';
+import {MemoryRouter, Router } from 'react-router-dom';
 import { createMemoryHistory } from 'history';
 import { Provider } from 'react-redux';
 import { store } from '../../setup/store';
@@ -10,8 +10,27 @@ import ProfileForm from './ProfileForm.js';
 jest.mock('../user/api/actions')
 
 describe('ProfileForm', () => {
-  it('should run a test', () => {
-    let test = 1;
-    expect(test).toEqual(1)
+
+  beforeEach(() => {
+    render(
+      <Provider store={store}>
+        <MemoryRouter>
+          <ProfileForm />
+        </MemoryRouter>
+      </Provider>
+    )
+  })
+  it('should display the ProfileForm\'s edit mode on load', () => {
+
+    const profileFormTest = screen.queryByTestId('profile');
+    const profileEditModeTest = screen.queryByTestId('profile-edit-mode');
+    const profileSaveButtonTest = screen.queryByTestId('profile-save-button');
+    
+    expect(profileFormTest).toBeInTheDocument();
+    expect(profileEditModeTest).toBeInTheDocument();
+    expect(profileSaveButtonTest).toBeInTheDocument();
+
+
+    screen.debug()
   })
 })
