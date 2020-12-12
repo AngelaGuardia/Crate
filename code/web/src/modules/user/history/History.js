acf0f2d34f2241e0f7cad36f19bb7186d4df2330
Blake Donnelly <blake.donnelly2@yahoo.com>
2020-12-12 14:45:10 -0500

    Add filtering process for products belonging to user history

--- code/web/src/modules/user/history/History.js
+++ code/web/src/modules/user/history/History.js
@@ -4,30 +4,19 @@ import Button from '../../../ui/button/Button';
 
 
 const History = (props) => {
-  let keptProducts = [];
-  props.deliveries.forEach(prod => {
-    if (prod.kept === true) {
-      keptProducts.push(prod)
-    }
+  let keptProducts = props.deliveries.filter(prod => {
+    prod.kept === true
   })
-  
-  // const allProducts = props.productsHistory.map(prod => {
-  //   //change this to return a Product component
-  //   //need to set up action creator to retrieve all Products 
-  //   //create array of productsHistory Ids to be cross referenced
-  //   //find elements within Products whose Id is included in ^
-  //   //connect the Products state to this History component
-  //   return (
-  //     <article>
-        
-  //     </article>
-  //   )
-  // })
+  let historyIds = props.deliveries.map(prod => prod.id)
+  let userProds = props.products.filter(prod => historyIds.includes(prod.id))
   return (
     <section style={{ textAlign: 'center', display: 'grid', gridTemplateRows: '1fr 1fr 1fr', paddingTop: '5em' }}>
       <h1 style={{ gridRowStart: '1' }}>My Product History</h1>
       <article style={{ gridRowStart: '2' }}>
-        {keptProducts.map(prod => <h1>{prod.productId}</h1>)}
+        {keptProducts.map(prod => <h1>{prod}</h1>)}
+      </article>
+      <article style={{ gridRowStart: '2' }}>
+        {userProds.map(prod => <h1>{prod}</h1>)}
       </article>
       <article style={{ marginTop: '5em', gridRowStart: '3', display: 'flex', justifyContent: 'space-around' }}> 
         <Button theme='secondary'>All</Button>
@@ -40,9 +29,10 @@ const History = (props) => {
 
 function historyState(state) {
   return {
-    deliveries: state.user.deliveries
+    deliveries: state.user.deliveries,
+    products: state.user.products
   }
 }
 
-export default connect(historyState)(History)
+export default connect(historyState )(History)
 
