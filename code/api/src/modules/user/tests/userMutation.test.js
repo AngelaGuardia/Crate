df4dc112aa14cf0395ddb86a90830f3fbf0daee9
Angela Guardia <angelaguardia20@gmail.com>
2020-12-12 15:06:48 -0800

    User Mutation tests

--- code/api/src/modules/user/tests/userMutation.test.js
+++ code/api/src/modules/user/tests/userMutation.test.js
@@ -17,80 +17,57 @@ describe('User Mutations', () => {
     )
   })
 
-  it('Checks the current number of users is 5', async (done) => {
-      const response1 = await request(server)
-        .post('/graphql')
-        .send({query: `{ users { email } }`})
-        .expect(200)
-      const initialUsers = response1.body.data.users.length
-      console.log(models.User.findAll())
-      expect(response1.body.data.users.length).toBe(initialUsers);
-      done();
-    })
-
-  it('Creates new user', async (done) => {
+  it('Creates a new user', async (done) => {
+    const initialUsers = await models.User.findAll({}).map(n => n.get({ plain: true}))
     const response2 = await request(server)
       .post('/graphql')
       .send({query: `mutation {userSignup(name: "Nicolas", email: "nick@gmail.com", password: "123456"){id}}`})
       .expect(200)
+    const newUsers = await models.User.findAll({}).map(n => n.get({ plain: true}))
+
+    expect(newUsers.length).toBe(initialUsers.length + 1)
     const id = response2.body.data.userSignup.id
-    const newUser = models.User.findOne({where: { id }})
+    const newUser = await models.User.findOne({where: { id }})
     expect(newUser.name).toBe("Nicolas");
     done();
   })
 
-  it('Checks the number of users increased to 6', async (done) => {
-  const response3 = await request(server)
-    .post('/graphql')
-    .send({query: `{ users { email } }`})
-    .expect(200)
-  const newUsers = response3.body.data.users.length
-  expect(newUsers).toBe(initialUsers + 1);
-  done();
-  })
+  it.skip('Updates user information', async (done) => {
+    let user = await models.User.findOne({where: { email: "nick@gmail.com" }});
+    const oldName = user.dataValues.name;
+    expect(oldName).toBe("Nicolas");
+    const userId = user.dataValues.id;
 
-  it('Check that the new user has the corrent attributes', async (done) => {
-  const response4 = await request(server)
-    .post('/graphql')
-    .send({query: `{user(id:${newUserId}) { email name } }`})
-    .expect(200)
-  expect(response4.body).toMatchObject({
-    data:{
-      user: {
-        email: 'nick@gmail.com',
-        name: 'Nicolas',
-      }
-    }
-  });
-  expect(response4.body.data.user.email).toBe('nick@gmail.com');
-  done();
-  })
+    const response5 = await request(server)
+      .post('/graphql')
+      .send({query: `mutation {userUpdate(id: ${userId}, name: "Cam", email: "c@gmail.com"){id name email}}`})
+      .expect(200)
 
-  it('Updates user information', async (done) => {
-  const response5 = await request(server)
-    .post('/graphql')
-    .send({query: `mutation {userUpdate(id: ${newUserId}, name: "Cam", email: "c@gmail.com"){id name email}}`})
-    .expect(200)
-  expect(response5.body.data.userUpdate.name).toBe("Cam");
-  expect(response5.body.data.userUpdate.email).toBe("c@gmail.com");
-  done();
+    const updatedUser = await models.User.findOne({where: { id: userId }})
+
+    const newName = user.dataValues.name
+    expect(newName).toBe("Cam");
+
+    const newEmail = user.dataValues.name
+    expect(newEmail).toBe("c@gmail.com");
+    done();
   })
 
   it('Removes user', async (done) => {
-  const response6 = await request(server)
-    .post('/graphql')
-    .send({query: `mutation {userRemove(id:${newUserId}){id}}`})
-    .expect(200)
-  expect(response6.body.data.userRemove.id).toBe(Null);
-  done();
-  })
+    let user = await models.User.findOne({where: { email: "nick@gmail.com" }});
 
-  it('Checks that the number of users is back to 5', async (done) => {
-  const response7 = await request(server)
-    .post('/graphql')
-    .send({query: `{ users { email } }`})
-    .expect(200)
-  expect(response7.body.data.users.length).toBe(newUsers - 1);
-  done();
+    const userId = user.dataValues.id;
+
+    const initialUsers = await models.User.findAll({}).map(n => n.get({ plain: true}))
+
+    const response6 = await request(server)
+      .post('/graphql')
+      .send({query: `mutation {userRemove(id:${userId}){id}}`})
+      .expect(200)
+
+    const newUsers = await models.User.findAll({}).map(n => n.get({ plain: true}))
+
+    expect(newUsers.length).toBe(initialUsers.length - 1)
+    done();
   })
 })
